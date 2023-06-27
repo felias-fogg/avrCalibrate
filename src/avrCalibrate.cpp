ddb7e2cbd0d1771eb03c10afd27791359f9eb547
Bernhard Nebel <nebel@hinterm-ziel.de>
2023-06-27 21:24:36 +0200

    V0.6.2

--- src/avrCalibrate.cpp
+++ src/avrCalibrate.cpp
@@ -1,7 +1,12 @@
 // library for setting calibration values at setup
 #include <avrCalibrate.h>
 
-void avrCalibrate::init(int osccal, int intref)
+void avrCalibrate::init(int osccal,
+#if !defined(__AVR_ATtiny2313__) && !defined(__AVR_ATtiny2313A__) && !defined(__AVR_ATtiny4313__)
+			int intref)
+#else
+                        __attribute__ ((unused)) int intref)
+#endif
 {
   if (osccal >= 0 && osccal <= 0xFF) // only legal OSCCAL values!
     OSCCAL = osccal;
@@ -14,5 +19,9 @@ void avrCalibrate::init(int osccal, int intref)
 void avrCalibrate::init(void)
 {
   init(eeprom_read_byte((byte *)(E2END-1)) == 0 ? eeprom_read_byte((byte *)E2END) : NOOSCCAL,
+#if !defined(__AVR_ATtiny2313__) && !defined(__AVR_ATtiny2313A__) && !defined(__AVR_ATtiny4313__)
        (int)eeprom_read_word((uint16_t *)EE_INTREF));
+#else
+       NOVOLT);
+#endif
 }
